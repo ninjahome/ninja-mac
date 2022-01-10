fc984e297b67d26ff7e5b4e5f41f6d009571a792
hopwesley <ws@sinoc.io>
2022-01-10 13:06:23 +0800

    init

---
+++ ninja/ninja/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ninja
+//
+//  Created by wesley on 2021/12/14.
+//
+
+import Cocoa
+import ninjaLib
+
+class ViewController: NSViewController {
+
+        override func viewDidLoad() {
+                super.viewDidLoad()
+                // Do any additional setup after loading the view.
+        }
+
+        override var representedObject: Any? {
+                didSet {
+                // Update the view, if already loaded.
+                }
+        }
+
+
+}
+

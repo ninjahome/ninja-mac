7a4d31b32a56061bb99d404eb8138327c1a2c1c0
hopwesley <ws@sinoc.io>
2022-01-11 20:11:31 +0800

    side bar

--- ninja-mac/ninja-mac/SideBar.swift
+++ ninja-mac/ninja-mac/SideBar.swift
@@ -8,40 +8,30 @@
 import SwiftUI
 
 struct SideBar: View {
+        
     var body: some View {
+            
             VStack(alignment: .center) {
                     Image("logo").resizable().frame(width: 40, height: 40)
                     .overlay {
                             Circle().stroke(.purple, lineWidth: 2)
                     }
                     .shadow(radius: 4)
-                    Group{
-                    Button(action: {
-                            
-                    }) {
-                            Image("message_normal").resizable()                    }
-                            
-                    Button(action: {
-                            
-                    }) {
-                            Image("contact_normal").resizable()
-                    }
                     
-                    Button(action: {
-                            
-                    }) {
+                    Group{
+                            Image("message_normal").resizable()
+                            Image("contact_normal").resizable()
                             Image("user_normal").resizable()
                     }
-                    }
                     .frame(width: 35, height: 35)
-                    .buttonStyle(.plain)
-                    
+                    .padding()
                     
                     Spacer()
             }
             .padding(.all)
             .frame(width: 60)
     }
+        
 }
 
 struct SideBar_Previews: PreviewProvider {

d356a12e32aab546b3a11db80cdddb305183f3ae
hopwesley <ws@sinoc.io>
2022-01-18 11:35:18 +0800

    refactor style

--- ninja-mac/ninja-mac/View/message/MessageBody.swift
+++ ninja-mac/ninja-mac/View/message/MessageBody.swift
@@ -12,47 +12,47 @@ struct MessageBody: View {
         @Namespace var bottomID
         @State var showIndicator:Bool = true
         
-    var body: some View {
-            VSplitView{
-            ScrollViewReader { proxy in
-                    ScrollView(.vertical, showsIndicators: showIndicator) {
-                        Button("Scroll to Bottom") {
-                            withAnimation {
-                                proxy.scrollTo(bottomID)
-                            }
+        var body: some View {
+                VSplitView{
+                        ScrollViewReader { proxy in
+                                ScrollView(.vertical, showsIndicators: showIndicator) {
+                                        Button("Scroll to Bottom") {
+                                                withAnimation {
+                                                        proxy.scrollTo(bottomID)
+                                                }
+                                        }
+                                        .id(topID)
+                                        
+                                        VStack(spacing: 0) {
+                                                ForEach(0..<100) { i in
+                                                        if i % 2 == 0{
+                                                                Text("sss").id(i)
+                                                        }else{
+                                                                Text("bbbb").id(i)
+                                                        }
+                                                }
+                                        }
+                                        
+                                        Button("Top") {
+                                                withAnimation {
+                                                        proxy.scrollTo(topID)
+                                                }
+                                        }
+                                        .id(bottomID)
+                                }
+                                //                    .frame(maxWidth: .infinity, maxHeight: .infinity).background(.red)
                         }
-                        .id(topID)
-
-                        VStack(spacing: 0) {
-                            ForEach(0..<100) { i in
-                                    if i % 2 == 0{
-                                            Text("sss").id(i)
-                                    }else{
-                                            Text("bbbb").id(i)
-                                    }
-                            }
-                        }
-
-                        Button("Top") {
-                            withAnimation {
-                                proxy.scrollTo(topID)
-                            }
-                        }
-                        .id(bottomID)
-                    }
-//                    .frame(maxWidth: .infinity, maxHeight: .infinity).background(.red)
-            }
-            .padding(.all, 0.0)
-            .frame(maxWidth: .infinity, maxHeight: .infinity).background(.red)
-                    
-                    MessageInput()
-                            .frame(maxWidth: .infinity, minHeight: 150, idealHeight: 200, maxHeight: 300)
-            }
-    }
+                        .padding(.all, 0.0)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity).background(.red)
+                        
+                        MessageInput()
+                                .frame(maxWidth: .infinity, minHeight: 150, idealHeight: 200, maxHeight: 350)
+                }
+        }
 }
 
 struct ChatBody_Previews: PreviewProvider {
-    static var previews: some View {
-        MessageBody()
-    }
+        static var previews: some View {
+                MessageBody()
+        }
 }
--- ninja-mac/ninja-mac/View/message/MessageInput.swift
+++ ninja-mac/ninja-mac/View/message/MessageInput.swift
@@ -9,34 +9,44 @@ import SwiftUI
 
 struct MessageInput: View {
         @State var messageToSend:String=""
-
+        
         var body: some View {
                 VStack{
                         HStack{
                                 Button(action: addItem) {
-                                        Image(systemName: "face.smiling").font(.title2)
+                                        Image(systemName: "face.smiling")
+                                                .font(.custom("HelveticaNeue", size: 18))
                                 }
                                 Button(action: addItem) {
-                                        Image(systemName: "folder").font(.title2)
+                                        Image(systemName: "folder")
+                                                .font(.custom("HelveticaNeue", size: 18))
                                 }
                                 Button(action: addItem) {
-                                        Image(systemName: "mic").font(.title2)
+                                        Image(systemName: "mic")
+                                                .font(.custom("HelveticaNeue", size: 18))
                                 }
                                 Spacer()
                                 Button(action: addItem) {
-                                        Image(systemName: "ellipsis.bubble").font(.title2)
+                                        Image(systemName: "ellipsis.bubble")
+                                                .font(.custom("HelveticaNeue", size: 18))
                                 }
                         }
-                        .frame(height: 30)
+                        .frame(height: 40)
                         .buttonStyle(.borderless)
-                        .padding(EdgeInsets(top: 5, leading: 15, bottom: 5, trailing: 15))
+                        .padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
                         
                         TextEditor(text: $messageToSend)
-                                .font(.title3)
-                                .buttonStyle(.borderless)
-                                .padding(.trailing, -20)
-                                .onSubmit {}
-                                .disableAutocorrection(true).background(ignoresSafeAreaEdges: .trailing)
+                                .foregroundColor(Color.black)
+                                .lineSpacing(5)
+                                .font(.custom("HelveticaNeue", size: 13))
+                        //                                .font(.title3)
+                                .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: -20))
+                                .onSubmit {
+                                        sendMsg()
+                                }
+                                .border(Color.black, width: 1)
+                                .disableAutocorrection(true)
+                        Spacer()
                 }
                 .background(Color(red: 0.952, green: 0.952, blue: 0.952))
         }
@@ -44,6 +54,9 @@ struct MessageInput: View {
         
         private func addItem(){
                 print("Edit button was tapped")
+        }
+        private func sendMsg(){
+                print("prepare tor send message")
         }
 }
 

b4721480ce334333a1fa0c79d5452927f5641032
hopwesley <ws@sinoc.io>
2022-01-19 16:23:03 +0800

    refactor logic

---
+++ ninja-mac/ninja-mac/View/CreateAccountView.swift
@@ -0,0 +1,20 @@
+//
+//  CreateAccount.swift
+//  ninja-mac
+//
+//  Created by wesley on 2022/1/19.
+//
+
+import SwiftUI
+
+struct CreateAccountView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreateAccount_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateAccountView()
+    }
+}
---
+++ ninja-mac/ninja-mac/View/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  Login.swift
+//  ninja-mac
+//
+//  Created by wesley on 2022/1/19.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Login_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
